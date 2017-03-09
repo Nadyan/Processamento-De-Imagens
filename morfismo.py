a7cc6c7a71bb50d78f858e37b8521681d896f1cf
Nadyan <nadyan_sp@hotmail.com>
2017-03-09 15:25:22 -0300

    MorfismoDone

--- morfismo.py
+++ morfismo.py
@@ -1,8 +1,8 @@
 from PIL import Image
 
 print("Abrindo Imagens...")
-pil1 = Image.open('C:\Users\Nadyan\Desktop\PIM - T1\happy.jpg')
-pil2 = Image.open('C:\Users\Nadyan\Desktop\PIM - T1\angry.jpg')
+pil1 = Image.open('happy.jpg')
+pil2 = Image.open('angry.jpg')
 
 (l1, h1) = pil1.size
 (l2, h2) = pil2.size
@@ -17,18 +17,29 @@ if l1 != l2 or h1 != h2: #confere se as duas imagens possuem a mesma dimensao
 (l, h) = (l1, h1) #atribui a dimensao da imagem de saida
 
 u1 = 0.4 #parametro para interpolacao da imagem 1
-u2 = 0.6 #parametro para interpolacao da imagem 2
+u2 = 0.8 #parametro para interpolacao da imagem 2
 
-out = Image.new(pil1.mode, (l, h))
+out1 = Image.new(pil1.mode, (l, h))
+out2 = Image.new(pil1.mode, (l, h))
 
 print("Calculando...")
 for j in range(0, h):
     for i in range(0, l):
-        u = (1-u1) * pil1.getpixel(i, j) + u1 * pil2.getpixel(i, j) #interpolacao dos pixels
-        out1.putpixel((i,j), u) #insere a interpolacao na imagem de saida
+        # INTERPOLACAO PARA A IMAGEM 1:
+        x = (tuple([(1-u1) * x for x in pil1.getpixel((i, j))])) # primeira parte da interpolacao com u1
+        y = (tuple([u1 * x for x in pil2.getpixel((i, j))]))     # segunda parte da interpolacao com u1
+        u = tuple(map(sum, zip(x, y)))                           # soma das partes da interpolacao
+        u = (tuple([int(x) for x in u]))                         # passar a tupla u de float para int
 
-        u = (1-u2) * pil1.getpixel(i, j) + u2 * pil2.getpixel(i, j)
-        out2.putpixel((i, j), u)
+        out1.putpixel((i,j), u)                                  # insere os pixels interpolados na imagem1
+
+        # INTERPOLACAO PARA A IMAGEM 2:
+        x = (tuple([(1-u2) * x for x in pil1.getpixel((i, j))])) # primeira parte da interpolacao com u2
+        y = (tuple([u2 * x for x in pil2.getpixel((i, j))]))     # segunda parte da interpolacao com u2
+        u = tuple(map(sum, zip(x, y)))                           # soma das partes da interpolacao
+        u = (tuple([int(x) for x in u]))                         # passar a tupla u de float para int
+        
+        out2.putpixel((i, j), u)                                 # insere os pixels interpolados na imagem2
 
 #out.show()
 out1.save("outMorph1.jpg", "JPEG")
