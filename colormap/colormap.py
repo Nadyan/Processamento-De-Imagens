e47432d1c70f91794907fe48e63b7e9a6493968c
Nadyan <nadyan_sp@hotmail.com>
2017-03-30 11:53:52 -0300

    separadoEmPastas

---
+++ colormap/colormap.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from numpy.random import randn
+from PIL import Image
+
+import cv2
+from matplotlib import pyplot as plt
+
+m = list()
+fh = open("mapa.txt", "r")
+for line in fh: 
+    aux = line.split() # quebra a linha em uma 'list' de strings
+
+    # monta um vetor com as strings individuais lidas por linhas
+    for i, val in enumerate(aux):
+        b = np.fromstring(val, dtype=int, sep=' ')
+        m.append(b)
+
+M = np.asarray(m)
+
+# converte o vetor M de 256*3 elementos em um array 256x3
+M = np.reshape(M,(256,3))
+
+#print(M)
+
+im = Image.open('mapaEUA.jpg')
+out = Image.new("RGB", im.size)
+height,width = im.size
+
+for i in range(0,height):
+	for j in range(0, width):
+		pixel = im.getpixel((i,j))
+		out.putpixel((i,j), (M[pixel][0], M[pixel][1], M[pixel][2]))
+
+out.save("pseudoCorOutEUA.jpg", "JPEG")
+
+
+# histograma:
+# pip install opencv-python
+gray_img = cv2.imread('mapaEUA.jpg', cv2.IMREAD_GRAYSCALE)
+#cv2.imshow('GoldenGate',gray_img)
+hist = cv2.calcHist([gray_img],[0],None,[256],[0,256])
+plt.hist(gray_img.ravel(),256,[0,256])
+plt.title('Histograma para escala de cinza mapaEUA.jpg')
+plt.show()
+		
+
---
+++ morfismo/morfismo.py
@@ -0,0 +1,48 @@
+from PIL import Image
+
+print("Abrindo Imagens...")
+pil1 = Image.open('walt1.jpeg')                                  # primeira imagem da sequencia
+pil2 = Image.open('walt4.jpeg')                                  # quarta imagem da sequencia
+
+(l1, h1) = pil1.size
+(l2, h2) = pil2.size
+
+print("Conferindo dimensoes...")
+if l1 != l2 or h1 != h2:                                         # confere se as duas imagens possuem a mesma dimensao
+    pint("ERRO! Imagens de dimensoes diferentes!")
+    print(l1, h1)
+    print(l2, h2)
+    exit()
+
+(l, h) = (l1, h1)                                                # atribui a dimensao da imagem de saida
+
+u1 = 0.4                                                         # parametro para interpolacao da imagem 1
+u2 = 0.8                                                         # parametro para interpolacao da imagem 2
+
+out1 = Image.new(pil1.mode, (l, h))
+out2 = Image.new(pil1.mode, (l, h))
+
+print("Calculando...")
+for j in range(0, h):
+    for i in range(0, l):
+        # INTERPOLACAO PARA A IMAGEM 1:
+        x = (tuple([(1-u1) * x for x in pil1.getpixel((i, j))])) # primeira parte da interpolacao com u1
+        y = (tuple([u1 * x for x in pil2.getpixel((i, j))]))     # segunda parte da interpolacao com u1
+        u = tuple(map(sum, zip(x, y)))                           # soma das partes da interpolacao
+        u = (tuple([int(x) for x in u]))                         # passar a tupla u de float para int
+
+        out1.putpixel((i,j), u)                                  # insere os pixels interpolados na imagem1
+
+        # INTERPOLACAO PARA A IMAGEM 2:
+        x = (tuple([(1-u2) * x for x in pil1.getpixel((i, j))])) # primeira parte da interpolacao com u2
+        y = (tuple([u2 * x for x in pil2.getpixel((i, j))]))     # segunda parte da interpolacao com u2
+        u = tuple(map(sum, zip(x, y)))                           # soma das partes da interpolacao
+        u = (tuple([int(x) for x in u]))                         # passar a tupla u de float para int
+        
+        out2.putpixel((i, j), u)                                 # insere os pixels interpolados na imagem2
+
+#out.show()
+out1.save("walt2.jpg", "JPEG")                                   # segunda imagem da sequencia
+out2.save("walt3.jpg", "JPEG")                                   # terceira imagem da sequencia
+
+print("Pronto! Arquivos de saida: walt2.jpg e walt3.jpg")
