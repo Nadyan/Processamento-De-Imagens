d21fb9fbcb88990a22813082049fbc0066bea86b
Nadyan <nadyan_sp@hotmail.com>
2017-04-19 11:16:55 -0300

    testar gray2rgb

--- realce/colormap.py
+++ realce/colormap.py
@@ -3,9 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 from numpy.random import randn
 from PIL import Image
-
 import cv2
-from matplotlib import pyplot as plt
 
 m = list()
 fh = open("grayscale_map.txt", "r")
@@ -22,20 +20,24 @@ M = np.asarray(m)
 # converte o vetor M de 256*3 elementos em um array 256x3
 M = np.reshape(M,(256,3))
 
-im = Image.open('aorta2.jpg')
-out = Image.new("L", im.size) # L = 8bits black and white 0-255
+im = Image.open('aorta1.jpg')
+backtorgb = cv2.cvtColor(im,cv2.COLOR_GRAY2RGB) # converte grayscale em RGB (testar)
+
+out = Image.new("RGB", im.size)
 height,width = im.size
 
 for i in range(0,height):
-	for j in range(0, width):
+	for j in range(0,width):
 		pixel = im.getpixel((i,j))
-		out.putpixel((i,j), (M[pixel][0], M[pixel][1], M[pixel][2]))
+		out.putpixel((i,j), (M[pixel][0], M[pixel][1], M[pixel][2])) # IndexError: index 82 is out of bounds for axis 1 with size 3 (WHYYYYYYYYYYY?)
 
-out.save("aorta2eq.jpg", "JPEG")
+out.save("aorta1_Out.jpg", "JPEG")
 
 # histograma equalizado:
-gray_img = cv2.imread('aorta2eq.jpg', cv2.IMREAD_GRAYSCALE)
-hist = cv2.calcHist([gray_img],[0],None,[256],[0,256])
-plt.hist(gray_img.ravel(),256,[0,256])
-plt.title('Histograma aorta2eq.jpg')
-plt.show()
+#gray_img = cv2.imread('aorta2eq.jpg', cv2.IMREAD_GRAYSCALE)
+#hist = cv2.calcHist([gray_img],[0],None,[256],[0,256])
+#plt.hist(gray_img.ravel(),256,[0,256])
+#plt.title('Histograma aorta2eq.jpg')
+#plt.show()
+
+
